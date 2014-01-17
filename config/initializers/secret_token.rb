1d22324e89418bb2c0618f90421bc689af6adb90
Andreas <andreas.westh@gmail.com>
2014-01-17 13:13:06 +0100

    Default rails app. rails new Bifrost-Boeger

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BifrostBoeger::Application.config.secret_token = 'd0937709294dd760c1f2a4e2abab818880c1645d8bdde0ce6a0a73ff3c62a824be59f06f9c0ca392a52c0193a3d81ade3785a95e6360dec5c3ec289599b49664'
