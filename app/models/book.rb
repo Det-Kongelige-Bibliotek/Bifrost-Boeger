e55f09ca69dab918c3d2428ba21080456473003b
Andreas <andreas.westh@gmail.com>
2014-01-17 15:57:37 +0100

    switching from activemodel to active Fedora

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,3 @@
-class Book < ActiveRecord::Base
+class Book < ActiveFedora::Base
   attr_accessible :authorpid, :authors, :bookid, :booktype, :contributor_content, :contributor_id, :contributorrolecode, :contributorrolename, :creationdate, :digitalprotection, :filesize, :filetype, :identifier, :identifiertype, :images, :keynames, :maindescription, :namesbeforekey, :numberofpages, :price, :publicationdate, :publishername, :rightlist, :subjects, :title
 end
