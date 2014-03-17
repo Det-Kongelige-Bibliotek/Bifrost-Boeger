20af979ef907462e03ba3d55fb9bd9adf958ac6d
davidgrove73 <z9m6ae7P>
2014-03-17 13:07:52 +0100

    gs

--- spec/models/datastreams/e_book_mods_spec.rb
+++ spec/models/datastreams/e_book_mods_spec.rb
@@ -8,7 +8,18 @@ describe Datastreams::EBookMods do
   end
 
   it 'should expose book infomation' do
-    @ds.mods.title.should == ['En Formiddag hos Frederik den Store, historisk Charakteerbillede']
-
+    @ds.mods.titleNonSort.should == ['En ']
+    @ds.mods.title.should == ['Formiddag hos Frederik den Store']
+    @ds.mods.subTitle.should == [': historisk Charakteerbillede']
+    @ds.mods.publisher.should == ["Jordans' Forlag"]
+    @ds.mods.originPlace.should == ['Kbh']
+    @ds.mods.dateIssued.should == ['1859']
+    @ds.mods.languageISO.should == []
+    @ds.mods.languageText.should == []
+    @ds.mods.subjectTopic.should == []
+    @ds.mods.physicalExtent.should == ['188 s.']
+    @ds.mods.physicalLocation.should == ['57,-458-8°']
+    @ds.mods.url.should == ['http://example.com/mock-file.pdf']
+    @ds.mods.author.should == ['Mühlbach, Louise']
   end
 end
