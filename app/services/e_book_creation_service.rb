3d317d7e24409f9ee67cc089f42189075c6508c7
Andreas <andreas.westh@gmail.com>
2014-03-18 10:38:17 +0100

    using the new book

--- app/services/e_book_creation_service.rb
+++ app/services/e_book_creation_service.rb
@@ -2,7 +2,7 @@ class EBookCreationService
 
   def create_from_message(message_string)
     message_h = JSON.parse(message_string)
-    book = EBook.new
+    book = Book.new
     book.datastreams['descMetadata'].content = message_h['MODS']
     book.uuid = message_h['UUID']
     book.url = message_h['files'].values
