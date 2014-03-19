8123f68da96517603446c77a854d29fe361c1886
jatr <jatr@kb.dk>
2014-03-19 13:37:32 +0100

    Adding a test for problem with missing MODS

--- spec/services/e_book_creation_service_spec.rb
+++ spec/services/e_book_creation_service_spec.rb
@@ -13,11 +13,21 @@ describe 'Create e-book' do
 
   it 'should Create an e-book' do
     book = @service.create_from_message(@message)
-    book.should be_a EBook
-    EBook.find(book.pid).should be_a EBook
-    book.uuid.should eql "444cd730-3f15-0131-5772-0050562881f4"
+    book.should be_a Book
+    Book.find(book.pid).should be_a Book
+    book.uuid.should eql '444cd730-3f15-0131-5772-0050562881f4'
     book.url.should be_a Array
-    book.url.include?("http://www.kb.dk/e-mat/dod/130020709562.pdf").should be_true
+    book.url.include?('http://www.kb.dk/e-mat/dod/130020709562.pdf').should be_true
+  end
+
+  it 'should create an e-book with escaped XML characters in the MODS' do
+    message = File.read("#{Rails.root}/spec/fixtures/broken_json.json")
+    book = @service.create_from_message(message)
+    book.should be_a Book
+    Book.find(book.pid).should be_a Book
+    book.uuid.should eql '56b9c990-917f-0131-e333-000c29cc78f6'
+    book.url.should be_a Array
+    book.url.include?('http://www.kb.dk/e-mat/dod/130019448607.pdf').should be_true
   end
 
 end
