86df7e44edfe123ced6f47e768d3b024f76870b1
Ronan McHugh <mchugh.r@gmail.com>
2014-03-31 13:53:44 +0200

    TA1015 - refactoring of view to use configurable yaml with helper. no empty fields shown.

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -5,11 +5,26 @@ module CatalogHelper
   include Blacklight::CatalogHelperBehavior
 
   REX_CONFIG = YAML.load_file("#{Rails.root}/config/rex.yml")[Rails.env]
+  METADATA_CONFIG = YAML.load_file("#{Rails.root}/config/metadata.yml")
 
   #Function to create a REX search link for the digital book using the barcode
   # @param [String] barcode
   # @return [String] URL for REX book search
   def create_rex_physical_book_search_link(barcode)
     REX_CONFIG['rex_book_search_template_uri'].gsub('BARCODE', barcode.to_sentence.html_safe)
+  end
+
+  # this helper function allows us to configure the metadata display
+  # via a YAML file consisting of key (field label), value (solr field name)
+  # @return [String] dl consisting of metadata labels and values
+  def render_document_metadata
+    content_tag(:dl, class: 'dl-horizontal dl-invert') do
+      METADATA_CONFIG.each_pair do |key, value|
+        unless @document[value].nil? || @document[value].empty?
+          concat(content_tag(:dt, class: "blacklight-#{value}") { t key})
+          concat(content_tag(:dd) { @document[value].to_sentence.html_safe })
+        end
+      end
+    end
   end
 end
