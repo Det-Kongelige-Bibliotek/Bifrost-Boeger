cf9ed6e589efbed8d8e3a1313160f74c4ffa38f3
Hasse Feldthaus <hafe@kb.dk>
2014-04-03 16:37:08 +0200

    Bugfix: Fixed enter button in search field.

--- app/assets/javascripts/search_behaviour.js
+++ app/assets/javascripts/search_behaviour.js
@@ -10,5 +10,12 @@ $(document).ready(function(){
         var descr = $(this).children('a').text();
         $('#js_search_type_button').html(descr + " <span class='caret'></span>");
         $('input#js_search_field').val(name);
+    });
+
+    $('#appendedPrependedDropdownButton').on('keydown', function(e) {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            e.target.form.submit();
+        }
     })
 });
