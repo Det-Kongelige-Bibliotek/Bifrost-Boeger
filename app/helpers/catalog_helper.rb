c8cf728784303af2877978176aea554bbfdebe43
Hasse Feldthaus <hafe@kb.dk>
2014-04-04 16:32:26 +0200

    Added a quick'n'dirty fix on the initial ': ' in subtitles.
    This need to be rolled back, when Sigges script is not prepending a ': ' to all subtitles any longer.

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -26,6 +26,8 @@ module CatalogHelper
     title = @document['title_tesim'].to_sentence.html_safe if @document['title_tesim']
     titleExtended = @document['titleExtended_tesim'].to_sentence.html_safe if @document['titleExtended_tesim']
     subtitle = @document['subtitle_tesim'].to_sentence.html_safe if @document['subtitle_tesim']
+    # cut off any initial ": " if it is present. FIXME: This shouldn't be done here, but in Sigges script - it is here now solely for the presentation @ Easter
+    subtitle = subtitle.gsub(/^:\s(.*)/, '\1')
 
     if title and titleExtended and subtitle
       content_tag(:h1, title + titleExtended) + content_tag(:h2, subtitle)
