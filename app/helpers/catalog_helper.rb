447ace9f4b0eefda4441f8fbf0ac34a6b0b53f63
Ronan McHugh <mchugh.r@gmail.com>
2014-04-04 16:57:44 +0200

    translate language codes in facets.

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -90,4 +90,21 @@ module CatalogHelper
 
     html_escape t(document_show_fields(document)[field].label, default: t('kb.search.index.label'))
   end
+
+
+  # Removing the [remove] link and label class from the default selected facet display
+  def render_bifrost_selected_facet_value(facet_solr_field, item)
+    content_tag(:span, render_bifrost_facet_value(facet_solr_field, item, :suppress_link => true), :class => "selected")
+  end
+
+  # Override to remove the label class (easier integration with bootstrap)
+  # and handles arrays
+  def render_bifrost_facet_value(facet_solr_field, item, options ={})
+    if item.is_a? Array
+      render_array_facet_value(facet_solr_field, item, options)
+    end
+   display_val = I18n.t(facet_display_value(facet_solr_field, item).upcase, default: facet_display_value(facet_solr_field, item))
+    (link_to_unless(options[:suppress_link], display_val, add_facet_params_and_redirect(facet_solr_field, item.value), :class=>"facet_select") + " " + render_facet_count(item.hits)).html_safe
+  end
+
 end
