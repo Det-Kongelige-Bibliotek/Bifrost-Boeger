e56a3d111478c6b61c41f147c392f2af64b2745d
Hasse Feldthaus <hafe@kb.dk>
2014-04-09 17:44:06 +0200

    Fixed the tabindex bug in facets (with some javascript manipulating the tabindex attr).

--- app/assets/javascripts/search_behaviour.js
+++ app/assets/javascripts/search_behaviour.js
@@ -21,4 +21,21 @@ $(document).ready(function(){
             e.target.form.submit();
         }
     });
+
+    /*
+     * Toggles the tabindex on and off on facet_selects according to whether they are collapsed or expanded
+     * If it is called without parameters, it will invert the resulting tabindex (since using it as an eventhandler will pass on an event)
+     * We need this because the eventhandler is called BEFORE collapse/expand and therefore height=0 => tabindex=0
+     * When initializing the elements, height=0 => tabindex=-1 (because they are not about to expand). Gee - explain this better anyone? :-6
+     */
+    var toggleFacetSelectsTabOrder = function (e) {
+        var collapsePanel = $(this).closest('.accordion-heading').next('.collapse'),
+            shouldBeInTabIndex = collapsePanel.css('height') === '0px'; // a bit upside down, because this is BEFORE the transition
+            shouldBeInTabIndex = (typeof e === 'undefined') ? !shouldBeInTabIndex : shouldBeInTabIndex;
+            $('.facet_select', collapsePanel).attr('tabindex', (shouldBeInTabIndex ? '0' : '-1'));
+    };
+    $('.facets .accordion-toggle').each(function(index, facetHeader){ // initialize tabindex for facet headers
+        toggleFacetSelectsTabOrder.call(facetHeader);
+    });
+    $('.facets .accordion-toggle').click(toggleFacetSelectsTabOrder); // setup event listener on facet headers
 });
