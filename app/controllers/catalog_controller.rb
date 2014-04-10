04cfc52a90fe5cdf0af283846edabe609f36975c
davidgrove73 <z9m6ae7P>
2014-04-10 10:53:25 +0200

    removed search in 'subject' added serch in 'udgivelsesår'

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -155,11 +155,7 @@ class CatalogController < ApplicationController
       }
     end
 
-    # Specifying a :qt only to show it's possible, and so our internal automated
-    # tests can test it. In this case it's the same as
-    # config[:default_solr_parameters][:qt], so isn't actually neccesary.
-    config.add_search_field('Udgivelsesår') do |field|
-      field.qt = 'search'
+    config.add_search_field('dateIssued') do |field|
       field.solr_local_parameters = {
         :qf => '$dateIssued_qf',
         :pf => '$dateIssued_pf'
